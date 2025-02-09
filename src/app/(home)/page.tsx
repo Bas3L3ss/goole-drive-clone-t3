f9e6fa558bff057395e05665466dde39e65dbea4
Bas3L3ss <phamthen.hun060907@gmail.com>
2025-02-09 12:23:20 +0700

    fix deploy by fixing type errors

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,7 +6,8 @@ export default function HomePage() {
   return (
     <>
       <h1 className="mb-4 bg-gradient-to-r from-neutral-200 to-neutral-400 bg-clip-text text-5xl font-bold text-transparent md:text-6xl">
-        T3 Drive
+        Google Drive{" "}
+        <span className="text-xs text-muted-foreground">( clone )</span>
       </h1>
       <p className="mx-auto mb-8 max-w-md text-xl text-neutral-400 md:text-2xl">
         Secure, fast, and easy file storage for the modern web
